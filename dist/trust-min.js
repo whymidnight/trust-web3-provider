c50f0ea737c3e90c7a58ba6290988619fa9c663b
Dominic DiGiacomo <ddigiacomo@triptychlabs.io>
2022-08-19 16:51:29 -0700

    rename trustwallet

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98cd734950a3d164b203d6264958d1da6cb855736efddf71bda36684f160bf03
-size 885807
+oid sha256:7184d32bdd69f215bbb243b0f43893dbf51954533c408b0aee01e4a202618dd8
+size 882720
